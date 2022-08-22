b64118377c29291eddd5cf8ff5a3063664327ef4
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-08-22 19:48:34 +1000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,14 +5,6 @@ declare(strict_types=1);
 namespace skyss0fly\ServerVersionPM4;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\updater\UpdateInfo;
-use pocketmine\event\server\UpdateNotifyEvent;
-use pocketmine\Server;
-use pocketmine\utils\VersionString;
-use pocketmine\VersionInfo;
-use function date;
-use function strtolower;
-use function ucfirst;
 
-class main extends UpdateChecker{
+class main extends PluginBase{
 }
