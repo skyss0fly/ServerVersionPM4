5ad77ab176ef2a9f6be0a5aaf3392e1323274d5f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-08-22 19:55:28 +1000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,6 +5,26 @@ declare(strict_types=1);
 namespace skyss0fly\ServerVersionPM4;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\VersionInfo;
 
 class main extends PluginBase{
+  public function __toString() : string{
+		return $this->getFullVersion();
+   function onLoad(): void {
+		
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::MINECOIN_GOLD . "ServerVersionPM4 Has Successfully loaded!");
+		}
+	}
+	
+	 function onEnable(): void {
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::LIGHT_PURPLE . "Your server version is $this->getFullVersion()");
+		}
+	}
+	
+	 function onDisable(): void {
+		if ($this->debugMode) {
+			$this->getLogger()->info(TextFormat::DARK_AQUA . "Goodbye hope you had fun!!");
+		}
 }
